b0a35ba28416187e067babc9dae8642a62976e43
Ahmed Mahmoud <amahmoud033@gmail.com>
2022-10-04 13:54:23 +0200

    upgrade laravel 9

---
+++ storage/framework/views/df1e29aff70c9738b67df03c0a3762e9cde0e81a.php
@@ -0,0 +1,17 @@
+<?php $__env->startSection('content'); ?>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Quiz</div>
+
+                    <div class="card-body">
+                        <main-view ></main-view>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Volumes/My Data/CAT APPs/Sites/2022/Laravel_VuewJS_SPA_Q-A_game/resources/views/quiz.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/e4db2b4bae5e379a10999ddb2cae1788b1b9b24e.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="<?php echo e(app()->getLocale()); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: bold;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <?php if(Route::has('login')): ?>
+                <div class="top-right links">
+                    <?php if(auth()->guard()->check()): ?>
+                        <a href="<?php echo e(url('/quiz')); ?>">Home</a>
+                    <?php else: ?>
+                        <a href="<?php echo e(route('login')); ?>">Login</a>
+                        <a href="<?php echo e(route('register')); ?>">Register</a>
+                    <?php endif; ?>
+                </div>
+            <?php endif; ?>
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Quiz game
+                </div>
+
+                <div class="links">
+                    <?php echo QrCode::backgroundColor(106, 164, 104)->color(255, 255, 255)->size(400)->generate(route('quiz'));; ?>
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+<?php /**PATH /Volumes/My Data/CAT APPs/Sites/2022/Laravel_VuewJS_SPA_Q-A_game/resources/views/welcome.blade.php ENDPATH**/ ?>
